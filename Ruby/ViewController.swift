98601e6d5472352b09f2c17e362a4662fcd1127d
Ruben voor de Poorte <Ruby@MacBook-Pro-van-Ruben.local>
2017-04-03 20:44:31 +0200

    Initial Commit

---
+++ Ruby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ruby
+//
+//  Created by Ruben voor de Poorte on 03-04-17.
+//  Copyright © 2017 Ruby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
