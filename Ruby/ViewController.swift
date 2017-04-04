331759900e50c3e9b4874bef63ef1772f81c51df
Ruby <r.voordepoorte@gmail.com>
2017-04-04 16:09:41 +0200

    My first commit

--- Ruby/ViewController.swift
+++ Ruby/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Name: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func changetapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            Name.text = "You tapped the button 10 times!"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
